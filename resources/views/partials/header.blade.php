eccade34b63a7530e419d656834efa87188f6b3c
SirAlyon <alessiosada04@gmail.com>
2022-06-13 00:34:55 +0200

    edit route conditional in header.blade

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,14 +14,14 @@
                 </a>
                 <a href="" class="{{Route::currentRouteName() === 'products.characters' ? 'active' : '' }}">CHARACTERS</a>
                 <a href="{{ route('products.index') }}" class="{{ Route::currentRouteName() === 'products.index' || Route::currentRouteName() === 'products.show' ? 'active' : '' }}">COMICS</a>
-                <a href="" {{Route::currentRouteName() === 'products.movies' ? 'active' : '' }}>MOVIES</a>
-                <a href="" {{Route::currentRouteName() === 'products.tv' ? 'active' : '' }}>TV</a>
-                <a href="" {{Route::currentRouteName() === 'products.games' ? 'active' : '' }}>GAMES</a>
-                <a href="" {{Route::currentRouteName() === 'products.collectables' ? 'active' : '' }}>COLLECTABLES</a>
-                <a href="" {{Route::currentRouteName() === 'products.videos' ? 'active' : '' }}>VIDEOS</a>
-                <a href="" {{Route::currentRouteName() === 'products.fans' ? 'active' : '' }}>FANS</a>
-                <a href="" {{Route::currentRouteName() === 'products.news' ? 'active' : '' }}>NEWS</a>
-                <a href="" {{Route::currentRouteName() === 'products.shop' ? 'active' : '' }}>SHOP</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.movies' ? 'active' : '' }}">MOVIES</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.tv' ? 'active' : '' }}">TV</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.games' ? 'active' : '' }}">GAMES</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.collectables' ? 'active' : '' }}">COLLECTABLES</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.videos' ? 'active' : '' }}">VIDEOS</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.fans' ? 'active' : '' }}">FANS</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.news' ? 'active' : '' }}">NEWS</a>
+                <a href="" class="{{Route::currentRouteName() === 'products.shop' ? 'active' : '' }}">SHOP</a>
             </nav>
         </div>
     </div>
